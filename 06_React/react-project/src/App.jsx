8b34d31f14fca99ab187cb9fd3c46ce2383e7858
paiktmddnjs <paiktmddnjs@naver.com>
2025-11-25 09:48:53 +0900

    feat: add Product components

--- 06_React/react-project/src/App.jsx
+++ 06_React/react-project/src/App.jsx
@@ -2,15 +2,59 @@
 import { useState } from 'react'
 import JavaScript from './components/JavaScript'
 import './App.css'
-import Style from './components/Style'
+import styled from 'styled-components'
+import Product from './components/Product'
+
+const products = [{
+  product_id: 1,
+  product_name: "삼성 갤럭시 S25",
+  price: 2000000,
+  color: "블랙",
+}, {
+  product_id: 2,
+  product_name: "아이폰 16",
+  price: 1800000,
+  color: "실버",
+}, {
+  product_id: 3,
+  product_name: "LG 그램",
+  price: 2200000,
+  color: "화이트",
+}]
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`
+
+const Th = styled.th`
+  background-color: #8a8a8a;
+  color: white;
+  padding: 12px;
+  border: 1px solid #afafaf
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* const [count, setCount] = useState(0) */
 
   return (
     <>
       {/* <JavaScript /> */}
-      <Style />
+      {/* <Style /> */}
+
+      <Table>
+        <thead>
+          <tr>
+            <Th>번호</Th>
+            <Th>제품명</Th>
+            <Th>가격</Th>
+            <Th>색상</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(p => <Product key={p.product_id} product={p}/>)}
+        </tbody>
+      </Table>
     </>
   )
 }
